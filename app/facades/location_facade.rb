2f012ec0bd54d9b327713e69e2b1602ec8f71c04
loganjacob76 <nagolisback@gmail.com>
2021-06-13 19:28:57 -0600

    Add get coordinates functionality

--- app/facades/location_facade.rb
+++ app/facades/location_facade.rb
@@ -1,4 +1,9 @@
 class LocationFacade
   class << self
+    def get_coordinates(location)
+      data = LocationService.lat_and_long(location)
+      t = Location.new(data[:results].first[:locations].first[:displayLatLng])
+      binding.pry
+    end
   end
 end
