a26762109876a5c103b5d1ac342c8f83d84b4a1c
loganjacob76 <nagolisback@gmail.com>
2021-06-13 23:11:01 -0600

    Expose forecast endpoint

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,4 +1,6 @@
 class API::V1::ForecastsController < ApplicationController
-  def index
+  def show
+    forecast = ForecastFacade.weather_for_city(params[:location])
+    render json: ForecastSerializer.new(forecast)
   end
 end
