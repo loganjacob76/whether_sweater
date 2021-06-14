6de420709dcb97048c1eba22e8cc0295f0697fe8
loganjacob76 <nagolisback@gmail.com>
2021-06-13 23:10:51 -0600

    Get weather forecast

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,4 +1,9 @@
 class ForecastFacade
   class << self
+    def weather_for_city(location)
+      coordinates = LocationFacade.get_coordinates(location)
+      data = ForecastService.city_weather(coordinates)
+      Forecast.new(data)
+    end
   end
 end
--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -1,4 +1,25 @@
 class ForecastService
   class << self
+    def city_weather(location)
+      resp = conn.get('/data/2.5/onecall') do |faraday|
+        faraday.params['lat'] = location.lattitude
+        faraday.params['lon'] = location.longitude
+        faraday.params['units'] = 'imperial'
+        faraday.params['exclude'] = 'minutely,alerts'
+      end
+      parse_json(resp)
+    end
+    
+    private
+    
+    def conn
+      Faraday.new(url: 'http://api.openweathermap.org') do |faraday|
+        faraday.params['appid'] = ENV['openweather_key']
+      end
+    end
+
+    def parse_json(resp)
+      JSON.parse(resp.body, symbolize_names: true)
+    end
   end
 end
