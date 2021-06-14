5ddfa57bbd0f097010582c7fb4be2c895a01c053
loganjacob76 <nagolisback@gmail.com>
2021-06-14 09:45:55 -0600

    Route, controller, service

---
+++ app/services/book_service.rb
@@ -0,0 +1,20 @@
+class BookService
+  class << self
+    def find_books(location)
+      resp = conn.get('/search.json') do |faraday|
+        faraday.params['q'] = location
+      end
+      parse_json(resp)
+    end
+    
+    private
+    
+    def conn
+      Faraday.new(url: 'http://openlibrary.org')
+    end
+
+    def parse_json(resp)
+      JSON.parse(resp.body, symbolize_names: true)
+    end
+  end
+end
