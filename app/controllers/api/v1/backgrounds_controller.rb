2155f0cac092836aa62911e14e756dbe06f35055
loganjacob76 <nagolisback@gmail.com>
2021-06-14 18:01:48 -0600

    Update to acommodate bad location

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,7 +2,11 @@ class API::V1::BackgroundsController < ApplicationController
   def index
     if params[:location].present?
       image = BackgroundsFacade.find_image(params[:location])
-      render json: ImageSerializer.new(image)
+      if image.is_a? Hash
+        render json: image, status: :bad_request
+      else
+        render json: ImageSerializer.new(image)
+      end
     else
       render json: { errors: 'No location found' }, status: :bad_request
     end
