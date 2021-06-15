93588aff3438bdc59831e8419b444e863ff59fa5
loganjacob76 <nagolisback@gmail.com>
2021-06-14 20:44:43 -0600

    Add error helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  # protect_from_forgery with: :exception
+  # helper_method :current_user
+  # before_action :authorized
+
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+
+  # def authorized
+  #   not_found unless current_user
+  # end
+
+  # def not_found
+  #   render file: "#{Rails.root}/public/404.html", status: :not_found
+  # end
+
+  def error_message(errors)
+    errors.full_messages.join(', ')
+  end
 end
